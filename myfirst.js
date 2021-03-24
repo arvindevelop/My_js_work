7f466270bc7d459aa90c3a7faecb4029dc4e5a5c
arvindevelop <aks7631353674@gmail.com>
2021-03-24 19:13:06 +0530

    more js file for node js

--- myfirst.js
+++ myfirst.js
@@ -1,5 +1,10 @@
 var http = require('http');
+var dt = require('./myfirstmodule');
 http.createServer(function(req,res){
     res.writeHead(200, {'Content-Type':'text/html'});
+    res.write("Current date and time: <br>" + dt.myDateTime());
+    
     res.end('Hello Arvind!');
 }).listen(8080);
+
+
